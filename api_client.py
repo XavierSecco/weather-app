9cdbdfeac63c0a3aec35d0c2fb32e62abd8b2b5a
Gustavo Tempel <gustavo.tempel@southerncode.us>
2022-05-24 16:21:24 -0300

    forecast logic add

--- api_client.py
+++ api_client.py
@@ -5,6 +5,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 BASE_WEATHER_API_URL = "http://api.openweathermap.org/data/2.5/weather"
+BASE_FORECAST_API_URL = "http://api.openweathermap.org/data/2.5/forecast"
 
 
 def _get_api_key():
@@ -35,6 +36,23 @@ def build_weather_query(city_input, measurement_unit):
     return url
 
 
+def build_forecast_query(city_input, measurement_unit):
+    """Builds the URL for an API request to OpenWeather's forecast API.
+
+    Args:
+        city_input (List[str]): Name of a city as collected by argparse
+        imperial (bool): Whether or not to use imperial units for temperature
+
+    Returns:
+        str: URL formatted for a call to OpenWeather's city name endpoint
+    """
+    api_key = _get_api_key()
+
+    # units = "imperial" if imperial else "metric"
+    url = f"{BASE_FORECAST_API_URL}?q={city_input}&units={measurement_unit}&appid={api_key}"
+
+    return url
+
 def get_weather_data(query_url):
     """Makes an API request to a URL and returns the data as a Python object.
 
@@ -47,5 +65,31 @@ def get_weather_data(query_url):
 
     response = requests.get(query_url)
     data = json.loads(response.text)
-    
+
     return {"main": data["main"], "weather": data["weather"][0], "wind": data["wind"], "name": data["name"]}
+
+
+def get_forecast_data(query_url):
+    """Makes an API request to a URL and returns the data as a Python object.
+
+    Args:
+        query_url (str): URL formatted for OpenWeather's city name endpoint
+
+    Returns:
+        dict: Weather information for a specific city
+    """
+
+    response = requests.get(query_url)
+    data = json.loads(response.text)
+
+    forecasts = [
+        {
+            "dt": i["dt"],
+            "main": i["main"],
+            "weather": i["weather"][0],
+            "wind": i["wind"],
+            "rain": i.get("rain", {"3h": 0}),
+        } for i in data["list"]]
+    
+    
+    return {"list": forecasts, "name": data["city"]["name"]}
--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from flask import Flask, request
-from api_client import build_weather_query, get_weather_data
+from api_client import build_weather_query,build_forecast_query, get_weather_data, get_forecast_data
 
 app = Flask(__name__)
 
@@ -15,6 +15,20 @@ def display_weather_info():
         weather_data = get_weather_data(url)
 
         return weather_data
+
+@app.route('/get-forecast-data', methods=['POST'])
+def display_forecast_info():
+    content_type = request.headers.get('Content-Type')
+    if(content_type == 'application/json'):
+        json_data = request.get_json()
+        city_input = json_data["city"]
+        measurement_unit = json_data["measurement_unit"]
+
+    url = build_forecast_query(city_input, measurement_unit)
+    forecast_data = get_forecast_data(url)
+
+    return forecast_data
+
 
 if __name__ == '__main__':
     app.run(debug=True)
